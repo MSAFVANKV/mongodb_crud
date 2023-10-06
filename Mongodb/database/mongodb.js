18d446778e0b57c3f9117f52bc0c5abcd09cd56c
Muhammed Safvan KV <mskvphed@gmail.com>
2023-10-06 17:41:31 +0530

    connected mongo atles

--- Mongodb/database/mongodb.js
+++ Mongodb/database/mongodb.js
@@ -1,12 +1,23 @@
 const mongoose = require('mongoose')
+const dotenv=require('dotenv')
+dotenv.config({path:".env"});
+// mongoose.connect(process.env.MONGO_DB_CONNECTION)
+// .then(()=>{
+//     console.log("mongodb connected");
+// })
+// .catch(()=> {
+//     console.log("Connection failed!!");
+// })
 
-mongoose.connect(process.env.MONGO_DB_CONNECTION)
+// mongodb atles
+mongoose.connect(process.env.MONGODB_ATLES)
 .then(()=>{
-    console.log("mongodb connected");
+    console.log("mongodb atles connected");
 })
 .catch(()=> {
     console.log("Connection failed!!");
 })
+
 
 const LoginSchema = new mongoose.Schema({
     username:{
